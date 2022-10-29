383f56a24e811161199d89ee003f3dc1da2e7545
Berk Cohadar <berkcohadar1@gmail.com>
2022-10-29 13:30:00 -0700

    Project created.

---
+++ src/components/NavigationBar/index.jsx
@@ -0,0 +1,43 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const NavigationBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Skills</Nav.Link>
+            <Nav.Link href="#link">Projects</Nav.Link>
+            <span className='navbar-social'>
+              <div className='navbar-social-icons'>
+                <a href='#'><img src=""></img></a>
+                <a href='#'><img src=""></img></a>
+                <a href='#'><img src=""></img></a>
+              </div>
+            </span>
+{/* 
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
