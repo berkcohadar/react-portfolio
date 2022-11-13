67a9521cb9bcf3fd3753cb352458d4735b6b69ba
Berk Cohadar <berkcohadar1@gmail.com>
2022-11-13 00:47:59 -0800

    New project added. Projects component design update.

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import bto from "../../assets/projects/bto-store.png";
+import arbol from "../../assets/projects/arbol-dashboard.png";
+
 import partisepeti from "../../assets/projects/partisepeti-desktop2.png";
 import ProductCard from "./ProjectCard";
 export default function Projects() {
   const projects = [
     {
       title: "partisepeti.com",
-      description: "Multi-vendor e-commerce web application.",
+      description: (<p> Multi-vendor e-commerce web application. <br /> React.js, Next.js, Python Django, HTML/SASS <br /> React Hooks, Redux.js, Toastify.js, Axios </p>),
       img: partisepeti,
       url: "https://marketplace.scistoneprojects.com",
     },
     {
+      title: "Global Climate Analytics Dashboard",
+      description: (<p> Climate data analytics UI. <br /> React.js, Chart.js, MongoDB, HTML/SASS <br /> React Hooks,  Axios </p>),
+      img: arbol,
+      url: "https://berkcohadar.github.io/react-climate-app/",
+    },
+    {
       title: "btostore.co",
-      description: "E-commerce landing page.",
+      description: (<p> E-commerce landing page. <br /> React.js, HTML/SASS, Ajax, React Hooks </p>),
       img: bto,
       url: "https://btostore.co",
     },
@@ -29,7 +37,7 @@ export default function Projects() {
         <Row className="projects-row">
           {projects.map((project, index) => {
             return (
-                <ProductCard {...project} index={index} />
+              <ProductCard {...project} index={index} />
             );
           })}
         </Row>
